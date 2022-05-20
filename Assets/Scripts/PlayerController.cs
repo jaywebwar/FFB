6ad7fce42e7815f3943fe0e5e50ea1d7b2b2c548
Jay Warner <jaywebwar@gmail.com>
2022-05-20 16:49:02 -0400

    Added physics input similar to rocket league

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,7 +8,14 @@ public class PlayerController : MonoBehaviour
     private Rigidbody _rigidBody;
 
     [SerializeField] float accelerationForce;
-    [SerializeField] float topSpeed;
+    [SerializeField] float topDirectionalSpeed;
+    [SerializeField] float maxRotationalSpeed;
+    [SerializeField] float accelerationTorque;
+    [SerializeField] float torqueEndSmoothness;
+
+    float adjustPitch;
+    float adjustRoll;
+    float adjustYaw;
 
     // Start is called before the first frame update
     void Start()
@@ -16,15 +24,57 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.W) && _rigidBody.velocity.magnitude < topSpeed)
+        //Always active standard thrust using physics
+        if (Vector3.Dot(_rigidBody.velocity, transform.forward) < topDirectionalSpeed)
         {
-            _rigidBody.AddForce(Vector3.forward * accelerationForce);
+            _rigidBody.AddForce(transform.forward * accelerationForce, ForceMode.Acceleration);
         }
-        if(_rigidBody.velocity.magnitude > topSpeed)
+        else
         {
-            _rigidBody.velocity = Vector3.forward * topSpeed;
+            Vector3 currentDirectionalSpeed = Vector3.Dot(_rigidBody.velocity, transform.forward) * transform.forward;
+            Vector3 currentNonDirectionalSpeed = _rigidBody.velocity - currentDirectionalSpeed;
+            _rigidBody.velocity = currentNonDirectionalSpeed + transform.forward * topDirectionalSpeed;
+        }
+
+        //Input for adjusting pitch
+        adjustPitch = Input.GetAxis("Vertical");
+
+        //Input for adjusting yaw and roll
+        if (Input.GetButton("RollButton"))
+        {
+            adjustRoll = Input.GetAxis("Horizontal");
+            adjustYaw = 0.0f;
+        }
+        else
+        {
+            adjustYaw = Input.GetAxis("Horizontal");
+            adjustRoll = 0.0f;
+        }
+
+        //Adjust Pitch, Yaw, Roll using physics
+        if (adjustYaw == 0.0f && adjustPitch == 0.0f && adjustRoll == 0.0f)
+        {
+            _rigidBody.angularVelocity = Vector3.Lerp(_rigidBody.angularVelocity, Vector3.zero, torqueEndSmoothness);
+        }
+        else if(_rigidBody.angularVelocity.magnitude < maxRotationalSpeed)
+        {
+            Vector3 adjustVector = new Vector3(adjustPitch, adjustYaw, adjustRoll);
+
+
+            Debug.DrawRay(transform.position, transform.forward*10, Color.yellow);
+            Debug.DrawRay(transform.position, transform.right*10, Color.green);
+            Debug.DrawRay(transform.position, transform.up*10, Color.red);
+
+
+            _rigidBody.AddTorque((transform.right * adjustPitch) * accelerationTorque, ForceMode.Acceleration);
+            _rigidBody.AddTorque((transform.up * adjustYaw) * accelerationTorque, ForceMode.Acceleration);
+            _rigidBody.AddTorque((transform.forward * adjustRoll) * accelerationTorque, ForceMode.Acceleration);
+        }
+        else
+        {
+            _rigidBody.angularVelocity = _rigidBody.angularVelocity.normalized * maxRotationalSpeed;
         }
     }
 }
